da9b9b3754845d0f94d70c4d72a469790700e894
MickaelDDDDD <60393481+MickaelDDDDD@users.noreply.github.com>
2020-04-04 12:09:25 +0200

    Add files via upload

---
+++ Jeu.py
@@ -0,0 +1,35 @@
+﻿import pygame
+pygame.init()
+from monstre import Monstre
+
+
+#Création de la fenêtre :
+
+pygame.display.set_caption("test")
+ecran = pygame.display.set_mode((1080, 720))
+
+
+bg = pygame.image.load("fond.png")
+
+game = Game()
+monstre = Monstre()
+
+running = True
+
+#Boucle Infinie
+
+while running:
+    for event in pygame.event.get():
+
+        ecran.blit(bg, (0, 0))
+        ecran.blit(monstre.image,monstre.rect)
+        pygame.display.flip()
+
+        if event.type == pygame.QUIT:
+
+            running = False
+            pygame.quit()
+            print("jeu fermé")
+        #Mouvements Monstres
+
+        move()
---
+++ monstre.py
@@ -0,0 +1,19 @@
+﻿
+import pygame
+
+
+class Monstre(pygame.sprite.Sprite):
+
+    def init(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 5
+        self.velocity = 10
+        self.image = pygame.image.load('Personnage.png')
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        self.rect.x += self.velocity
+
+
