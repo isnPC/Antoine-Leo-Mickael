1927309d8e96f9b1bf8698ad1a2868b9493fdd39
MickaelDDDDD <60393481+MickaelDDDDD@users.noreply.github.com>
2020-04-07 16:00:48 +0200

    Add files via upload

---
+++ glace_chocolat.py
@@ -0,0 +1,34 @@
+﻿import pygame
+from player import Joueur
+
+player = Player()
+
+class Glace_chocolat(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 5
+        self.velocity = 5
+        self.image = pygame.image.load('chocolat.png')
+        self.rect = self.image.get_rect()
+
+    def mouvements(self):
+        equart_x = player.rect.x - self.rect.x
+        if equart_x < 0:
+            self.rect.x = self.rect.x - self.velocity
+        else:
+            self.rect.x = self.rect.x + self.velocity
+
+        equart_y = player.rect.y - self.rect.y
+        if equart_y < 0:
+            self.rect.y = self.rect.y + self.velocity
+        else:
+            self.rect.y = self.rect.y - self.velocity
+
+        pygame.time.delay(1000)
+
+    def attaque_chauve_souris(self):
+        if self.rect == player.rect:
+            player.health = player.health - self.attack
